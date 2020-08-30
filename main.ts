21dcd54d8ec667907eecd2ba04f01b5b1cbd02cc
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-08-31 07:21:07 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -534,7 +534,7 @@ namespace eureka_blocks_soro {
     }
   }
 
-  //% color="#009A00" weight=28 blockId=sonar_ping_3 block="Aポート|%sonar_quality| きょりが |%limit| cmより |%nagasa|" group="5_単体ユニットセンサー"
+  //% color="#009A00" weight=28 blockId=sonar_ping_3 block="Aポート　|%sonar_quality| きょりが |%limit| cmより |%nagasa|" group="5_単体ユニットセンサー"
   //% limit.min=0 limit.max=50
   export function sonar_ping_3(pin:eureka_tlp,sonar_quality:sonar_avg,limit: number ,nagasa:kyori) :boolean{
         if (sonar_quality　==sonar_avg.低速高精度){
